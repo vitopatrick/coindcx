f294477f1f100959adb903ce58d457ac5e76e8e9
theWarlock <patrickvito2012@gmail.com>
2023-10-12 00:27:18 +0100

    changed path name

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import BackedByInvestors from "@/components/home/back-investors";
 import Features from "@/components/home/features";
 import FirstStep from "@/components/home/first-step";
-import HomeHero from "@/components/home/hero";
+import HomeHero from "@/components/home/HeroHome";
 import RegisteredUsers from "@/components/home/registered-user";
 import Secure from "@/components/home/secure";
 
